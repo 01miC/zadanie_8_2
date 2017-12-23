b9a6761b329a7dda79d6bd52515dfe89df597182
Michal Dokupil <michal@dokupil.pl>
2017-12-23 21:56:32 +0100

    Add script85 with complete function

--- js/script.js
+++ js/script.js
@@ -6,3 +6,4 @@ console.log('Hello, ' + name);
 /* 
 komentarze
 */
+
---
+++ js/script85.js
@@ -0,0 +1,9 @@
+var wys = prompt('Podaj wysokość trójkąta w cm');
+var pods = prompt('Podaj podstawę trójkąta w cm');
+
+    triangleArea = wys*pods/2;
+
+
+
+
+document.getElementById("wynik").innerText = triangleArea;
